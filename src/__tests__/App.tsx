9db8450388b5352b7ad8677ce3bb410647d987e3
Yuki Yamamoto <zx275fire@gmail.com>
2021-01-22 18:09:10 +0900

    playgroundへのlogへ変更

--- src/__tests__/App.tsx
+++ src/__tests__/App.tsx
@@ -1,9 +1,9 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import App from "../components/App";
 import React from "react";
 import ReactDOM from "react-dom";
 
 test("<App />", () => {
   const { container, getByText } = render(<App />);
-  expect(true).toBeTruthy();
+  screen.logTestingPlaygroundURL();
 });
